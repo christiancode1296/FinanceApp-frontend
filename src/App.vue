b529a81c653e491861dfa3de0f26c03498fdcf80
Christian Püschel <christian.code1296@gmail.com>
2025-10-30 17:53:17 +0100

    Bugfixes and Connection to Backend

--- src/App.vue
+++ src/App.vue
@@ -5,10 +5,13 @@
 </template>
 
 <script>
+import axios from 'axios'; // ← hinzufügen!
 import StockCharts from './components/StockCharts.vue';
 
 const apiBase = process.env.VUE_APP_API_URL;
-axios.get(`${apiBase}/stocks/AAPL`);
+axios.get(`${apiBase}/stocks/AAPL`)
+    .then(res => console.log(res.data))
+    .catch(err => console.error("Fehler beim Backend-Call:", err));
 
 export default {
   components: {
--- vue.config.js
+++ vue.config.js
@@ -2,9 +2,8 @@ module.exports = {
     devServer: {
         proxy: {
             '/api': {
-                target: 'http://localhost:8080',
+                target: 'http://localhost:8080', // ← hier läuft dein Spring-Boot-Backend
                 changeOrigin: true,
-                pathRewrite: { '^/api': '' },
             },
         },
     },
